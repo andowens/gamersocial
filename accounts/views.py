6f4e7c65e5d52b8ad2dddbea1799e6aeea91dc03
aowens <aowens@rosenaviation.com>
2017-09-29 10:58:19 -0700

    Fixed a redirection bug when redirecting from signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView
 from . import forms
 
 
 class SignUp(CreateView):
     form_class = forms.UserCreateForm
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
--- simplesocial/urls.py
+++ simplesocial/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
 
+app_name = 'base'
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.HomePage.as_view(), name='home'),
