91d6dc9d51d0c24089011b0df3c975ae0a51d3ea
Andrew Owens <iseis360@gmail.com>
2017-10-02 21:57:36 -0700

    Did stuff

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class GroupMemberInline(admin.TabularInline):
+    models = models.GroupMember
+
+admin.site.register(models.Group)
+
+
